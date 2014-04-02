368c41777ecafaa9710716dc4436d8c54f1c6c68
Jeremy Clemans & Ali Abou-Zahr <student@epicodus.com>
2014-04-02 14:24:59 -0700

    next and previous page tests working

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,7 +2,12 @@ class Lesson < ActiveRecord::Base
   validates :name, :presence => true
 
   def next_lesson
-    sorted = Lesson.all.sort_by &:lesson_number
-    self.id
+    sorted = Lesson.order(lesson_number: :asc)
+    sorted.detect { |i| i.lesson_number > self.lesson_number }
+  end
+
+  def previous_lesson
+    sorted = Lesson.order(lesson_number: :desc)
+    sorted.detect { |i| i.lesson_number < self.lesson_number }
   end
 end
--- spec/lesson_spec.rb
+++ spec/lesson_spec.rb
@@ -4,11 +4,20 @@ describe Lesson do
   it { should validate_presence_of :name }
 
   describe 'next' do
-    it 'should return to next lesson' do
+    it 'should return the next lesson' do
       testlesson1 = Lesson.create({name: 'Ruby Basics', lesson_number: 101})
       testlesson2 = Lesson.create({name: 'Ruby Not basics', lesson_number: 105})
       testlesson3 = Lesson.create({name: 'Ruby Not bascicly', lesson_number: 90})
       testlesson1.next_lesson.should eq testlesson2
+    end
+  end
+
+  describe 'previous' do
+    it 'should return the previous lesson' do
+      testlesson1 = Lesson.create({name: 'Ruby Basics', lesson_number: 101})
+      testlesson2 = Lesson.create({name: 'Ruby Not basics', lesson_number: 105})
+      testlesson3 = Lesson.create({name: 'Ruby Not bascicly', lesson_number: 90})
+      testlesson2.previous_lesson.should eq testlesson1
     end
   end
 end
